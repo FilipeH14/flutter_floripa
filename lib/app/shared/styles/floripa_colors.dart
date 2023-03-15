2ae76b066a803aa44aefac77d9aca40471b1f90d
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-03-14 23:36:08 -0300

    definindo estilos de tipografia e cores

---
+++ lib/app/shared/styles/floripa_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FloripaColors {
+  static FloripaColors? _instance;
+
+  FloripaColors._();
+
+  static FloripaColors get i {
+    _instance ??= FloripaColors._();
+    return _instance!;
+  }
+
+  Color get primary => const Color(0XFF1E5BE4);
+  Color get secondary => const Color(0XFFBD2860);
+}
+
+extension FloripaColorsExtensions on BuildContext {
+  FloripaColors get colors => FloripaColors.i;
+}
---
+++ lib/app/shared/styles/floripa_styles.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class FloripaStyles {
+  static FloripaStyles? _instance;
+
+  FloripaStyles._();
+
+  static FloripaStyles get i {
+    _instance ??= FloripaStyles._();
+    return _instance!;
+  }
+
+  String get font => 'PTSans';
+
+  TextStyle get floripaRegular => TextStyle(
+        fontFamily: font,
+        fontWeight: FontWeight.normal,
+      );
+
+  TextStyle get floripaBold => TextStyle(
+        fontFamily: font,
+        fontWeight: FontWeight.w700,
+      );
+}
+
+extension FloripaStylesExtensions on BuildContext {
+  FloripaStyles get textStyles => FloripaStyles.i;
+}
